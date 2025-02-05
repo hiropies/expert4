32d2045293f0dc8133ba77ef87adc1798fd8fea1
hiroto <s223087@stn.nagaokaut.ac.jp>
2025-02-05 20:11:29 +0900

    実験が出来ます

--- current_control_ver2_new.c
+++ current_control_ver2_new.c
@@ -1615,6 +1615,7 @@ interrupt void ControlFunction(void)
 
             ＊軸によって違うので注意！！！（ギアとかによる）
           ***************************************************************************** */
+          start3 = (float)C6657_timer1_read() * 4.8e-9 * 1e6;
           // 1軸目 位置指令
           // ランプ関数生成関数で位置指令を決定
           // 引数 a:傾き、t_wait:開始時間、t_ramp:ランプアップ時間、t_const:定常時間
@@ -1629,7 +1630,6 @@ interrupt void ControlFunction(void)
             axis1.qm_ref = start_back1;
           }
 
-          LimitPosCmd(&axis1);
           // 1軸目 速度P制御
           axis1.wm_ref = (axis1.qm_ref_z2 - axis1.qm) * axis1.Kpp;
 
@@ -1669,7 +1669,6 @@ interrupt void ControlFunction(void)
             axis2.qm_ref = start_back2;
           }
 
-          LimitPosCmd(&axis2);
           // 2軸目 速度P制御
           axis2.wm_ref = (axis2.qm_ref_z2 - axis2.qm) * axis2.Kpp;
 
@@ -1702,7 +1701,6 @@ interrupt void ControlFunction(void)
           axis3.qm_ref_z1 = axis3.qm_ref;
           axis3.qm_ref = 1.0 * ManyRampGenerator3rdAxis(axis3.a_ramp, axis3.vel, axis3.t_start, axis3.t_ramp, axis3.t_const, axis3.a_ramp_back, axis3.t_ramp_back) + start_back3;
           // axis3.qm_ref = start_back3;
-          LimitPosCmd(&axis3);
 
           // 3軸目 速度P制御
           axis3.wm_ref = (axis3.qm_ref_z2 - axis3.qm) * axis3.Kpp;
@@ -1740,6 +1738,7 @@ interrupt void ControlFunction(void)
               flag_fin++;
             }
           }
+          WAVE_TimeP_PI = (float)C6657_timer1_read() * 4.8e-9 * 1e6 - start3;
         }
         else
         {
@@ -1747,7 +1746,6 @@ interrupt void ControlFunction(void)
           axis2.IrefQ = axis2.Icmp;
           axis3.IrefQ = axis3.Icmp;
         }
-
         // 制御周期の測定結果出力 ファンクションリファレンスp45より
         // 制御にかかった時間を測定している。
         WAVE_Timer1 = (float)C6657_timer1_read() * 4.8e-9 * 1e6;
@@ -3469,6 +3467,7 @@ void FDTD_Tm(Robot *robo)
 
 void FDTD_Tm_Init()
 {
+  
   Tm_sub[0].den_0 = 0.0;
   Tm_sub[0].den_1 = 0.0;
   Tm_sub[0].den_2 = (Tp * Tp * axis1.Ksn * axis1.Ktn) / axis1.Rgn / axis1.Jmn;
