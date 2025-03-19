68c78bc5a28a8322cfbfe17a3c147f3680c163d0
hiroto <s223087@stn.nagaokaut.ac.jp>
2025-03-19 11:30:20 +0900

    change wm_ref

--- current_control_ver2_new.c
+++ current_control_ver2_new.c
@@ -1605,7 +1605,11 @@ interrupt void ControlFunction(void)
           // axis1.qm_ref = motor_cmd[0];
           axis1.qm_ref = axis1.wm_cmd * Tp + axis1.qm_ref_z1;
           LimitPosCmd(&axis1);
-          axis1.wm_ref = (axis1.qm_ref_z2 - axis1.qm) * axis1.Kpp + axis1.Kff * axis1.wm_cmd - axis1.Kfb * axis1.wm;
+          if(flag_PPgain == 2){
+            axis1.wm_ref = (axis1.qm_ref_z2 - axis1.qm) * axis1.Kpp + axis1.Kff * axis1.wm_cmd - axis1.Kfb * axis1.wm;
+          }else{
+            axis1.wm_ref = (axis1.qm_ref_z2 - axis1.qm) * axis1.Kpp;
+          }
 
           if (flag_FF == 1)
           {
@@ -1643,7 +1647,14 @@ interrupt void ControlFunction(void)
           // axis2.qm_ref = motor_cmd[1];
           axis2.qm_ref = axis2.wm_cmd * Tp + axis2.qm_ref_z1;
           LimitPosCmd(&axis2);
-          axis2.wm_ref = (axis2.qm_ref_z2 - axis2.qm) * axis2.Kpp + axis2.Kff * axis2.wm_cmd - axis2.Kfb * axis2.wm;
+          if (flag_PPgain == 2)
+          {
+            axis2.wm_ref = (axis2.qm_ref_z2 - axis2.qm) * axis2.Kpp + axis2.Kff * axis2.wm_cmd - axis2.Kfb * axis2.wm;
+          }
+          else
+          {
+            axis2.wm_ref = (axis2.qm_ref_z2 - axis2.qm) * axis2.Kpp;
+          }
 
           if (flag_FF == 1)
           {
@@ -1681,7 +1692,14 @@ interrupt void ControlFunction(void)
           // axis3.qm_ref = motor_cmd[2];
           axis3.qm_ref = axis3.wm_cmd * Tp + axis3.qm_ref_z1;
           LimitPosCmd(&axis3);
-          axis3.wm_ref = (axis3.qm_ref_z2 - axis3.qm) * axis3.Kpp + axis3.Kff * axis3.wm_cmd - axis3.Kfb * axis3.wm;
+          if (flag_PPgain == 2)
+          {
+            axis3.wm_ref = (axis3.qm_ref_z2 - axis3.qm) * axis3.Kpp + axis3.Kff * axis3.wm_cmd - axis3.Kfb * axis3.wm;
+          }
+          else
+          {
+            axis3.wm_ref = (axis3.qm_ref_z2 - axis3.qm) * axis3.Kpp;
+          }
 
           // 3軸目 速度PI制御＋SFB
           if (flag_FF == 1)
