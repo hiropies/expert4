086e013d1c22a19fc1d7a1cb451dafd0aa67d710
hiroto <s223087@stn.nagaokaut.ac.jp>
2025-11-13 17:14:04 +0900

    速度制御系の統合を解除

--- current_control_ver2_new.c
+++ current_control_ver2_new.c
@@ -2422,9 +2422,7 @@ void MW_main(void)
 
   // 可変ゲインの極を指定する(係数図法)
   // 1軸目
-  // gsub[0].tau_cdm = 0.05;
-  // gsub[0].zw = 50.0;
-  gsub[0].tau_cdm = 0.058;
+  gsub[0].tau_cdm = 0.05;
   gsub[0].zw = 50.0;
   gsub[0].r_cdm1 = 2.5;
   gsub[0].r_cdm2 = 2.0;
@@ -2436,8 +2434,8 @@ void MW_main(void)
   gsub[0].r1_pole = 2.5;
 
   // 2軸目
-  gsub[1].tau_cdm = 0.058;
-  gsub[1].zw = 50.0;
+  gsub[1].tau_cdm = 0.05;
+  gsub[1].zw = 30.0;
   gsub[1].r_cdm1 = 2.5;
   gsub[1].r_cdm2 = 2.0;
   gsub[1].r_cdm3 = 2.0;
@@ -2449,7 +2447,7 @@ void MW_main(void)
 
   // 3軸目
   gsub[2].tau_cdm = 0.058;
-  gsub[2].zw = 50.0;
+  gsub[2].zw = 30.0;
   gsub[2].r_cdm1 = 2.5;
   gsub[2].r_cdm2 = 2.0;
   gsub[2].r_cdm3 = 2.0;
