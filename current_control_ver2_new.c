24e32b56a6acb3eba37c5240a9a2966e2d260adf
hiroto <s223087@stn.nagaokaut.ac.jp>
2025-11-13 16:40:29 +0900

    三軸がtau=0.05だと暴走するので0.058で統一

--- current_control_ver2_new.c
+++ current_control_ver2_new.c
@@ -2424,8 +2424,8 @@ void MW_main(void)
   // 1軸目
   // gsub[0].tau_cdm = 0.05;
   // gsub[0].zw = 50.0;
-  gsub[0].tau_cdm = 0.05;
-  gsub[0].zw = 30.0;
+  gsub[0].tau_cdm = 0.058;
+  gsub[0].zw = 50.0;
   gsub[0].r_cdm1 = 2.5;
   gsub[0].r_cdm2 = 2.0;
   gsub[0].r_cdm3 = 2.0;
@@ -2436,8 +2436,8 @@ void MW_main(void)
   gsub[0].r1_pole = 2.5;
 
   // 2軸目
-  gsub[1].tau_cdm = 0.05;
-  gsub[1].zw = 30.0;
+  gsub[1].tau_cdm = 0.058;
+  gsub[1].zw = 50.0;
   gsub[1].r_cdm1 = 2.5;
   gsub[1].r_cdm2 = 2.0;
   gsub[1].r_cdm3 = 2.0;
@@ -2448,8 +2448,8 @@ void MW_main(void)
   gsub[1].r1_pole = 2.5;
 
   // 3軸目
-  gsub[2].tau_cdm = 0.05;
-  gsub[2].zw = 30.0;
+  gsub[2].tau_cdm = 0.058;
+  gsub[2].zw = 50.0;
   gsub[2].r_cdm1 = 2.5;
   gsub[2].r_cdm2 = 2.0;
   gsub[2].r_cdm3 = 2.0;
